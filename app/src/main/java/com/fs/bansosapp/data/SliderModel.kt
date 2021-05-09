d5865a0be3b30b9d966aa4779175ca99cd9912c6
ianfyan <imamkurfian@gmail.com>
2021-05-09 19:10:05 +0700

    Splash, Slider and Login UI Commit

---
+++ app/src/main/java/com/fs/bansosapp/data/SliderModel.kt
@@ -0,0 +1,3 @@
+package com.fs.bansosapp.data
+
+data class SliderModel(val image: Int, val desc: String)
---
+++ app/src/main/java/com/fs/bansosapp/ui/activity/slider/SliderActivity.kt
@@ -0,0 +1,69 @@
+package com.fs.bansosapp.ui.activity.slider
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import androidx.viewpager2.widget.ViewPager2
+import com.fs.bansosapp.R
+import com.fs.bansosapp.data.SliderModel
+import com.fs.bansosapp.databinding.ActivitySliderBinding
+import com.fs.bansosapp.ui.activity.auth.LoginActivity
+import com.fs.bansosapp.ui.adapter.SliderAdapter
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
+
+class SliderActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivitySliderBinding
+    private lateinit var listSlider: ArrayList<SliderModel>
+    private lateinit var sliderPager: ViewPager2
+    private lateinit var indicator: TabLayout
+    private val slideHandler = Handler()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding =ActivitySliderBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        sliderPager = binding.pageSlider
+        indicator = binding.sliderIndicator
+
+        setupDataSlider()
+        setupSlider()
+
+        binding.sliderButton.setOnClickListener{
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        }
+    }
+
+    private fun setupDataSlider(){
+        listSlider = ArrayList()
+        listSlider.add(SliderModel(R.drawable.slider_1, getString(R.string.slider_1)))
+        listSlider.add(SliderModel(R.drawable.slider_2, getString(R.string.slider_2)))
+        listSlider.add(SliderModel(R.drawable.slider_3, getString(R.string.slider_3)))
+    }
+
+    private fun setupSlider(){
+        sliderPager.adapter = SliderAdapter(listSlider)
+        TabLayoutMediator(indicator, sliderPager){ tab, position ->
+        }.attach()
+
+        sliderPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback(){
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                slideHandler.removeCallbacks(sliderRunnable)
+                slideHandler.postDelayed(sliderRunnable, 5000)
+            }
+        })
+    }
+
+    private val sliderRunnable = Runnable {
+        if (sliderPager.currentItem <  2){
+            sliderPager.setCurrentItem(sliderPager.currentItem+1)
+        }else{
+            sliderPager.setCurrentItem(0)
+        }
+    }
+}
