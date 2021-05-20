c0dcb5915f97728d48aec250bc2580fd257e681d
ianfyan <imamkurfian@gmail.com>
2021-05-20 15:23:40 +0700

    Dashboard UI

---
+++ app/src/main/java/com/fs/bansosapp/ui/activity/home/HomeFragment.kt
@@ -0,0 +1,26 @@
+package com.fs.bansosapp.ui.activity.home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.fs.bansosapp.R
+import com.fs.bansosapp.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentHomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
